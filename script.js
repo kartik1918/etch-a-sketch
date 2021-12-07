ae4ff9cd0f46fa9298b585708523438f785cd99b
Kartik Gupta <kartikgupta966@gmail.com>
2021-12-07 14:02:13 +0530

    improved design

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const gridNumber = prompt("Enter Grid Number")
+
 const container = document.querySelector('.container');
-for (let j = 0; j < 16; j++) {
+for (let j = 0; j < gridNumber; j++) {
     container.style.cssText = "display: flex; flex-wrap: wrap";
-    for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-        container.appendChild(div);
-        div.style.cssText = "display: inline-block; min-width: 5%;"
+    for (let i = 0; i < gridNumber; i++) {
+    const divs = document.createElement('div');
+        container.appendChild(divs);
+        divs.style.cssText = `display: inline-block; min-width: 5%`;
+        divs.addEventListener('mouseover', function() {
+            divs.classList.toggle('color');
+        })
     }
     container.style.cssText = "display: flex; flex-wrap: wrap"
 }
-
-
-
-
