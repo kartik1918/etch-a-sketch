a51edf165c5b9785eaa45c8598ff725806182cf0
Kartik Gupta <kartikgupta966@gmail.com>
2021-12-07 14:32:48 +0530

    almost done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gridNumber = prompt("Enter Grid Number")
 
 const container = document.querySelector('.container');
-for (let j = 0; j < gridNumber; j++) {
+for (let j = 1; j < gridNumber; j++) {
     container.style.cssText = "display: flex; flex-wrap: wrap";
     for (let i = 0; i < gridNumber; i++) {
     const divs = document.createElement('div');
         container.appendChild(divs);
-        divs.style.cssText = `display: inline-block; min-width: 5%`;
+        divs.style.width = `${450 / gridNumber}px`;
+        divs.style.height = `${490 / gridNumber}px`;
+        // divs.style.cssText = `display: inline-block; min-width: 5%`;
         divs.addEventListener('mouseover', function() {
             divs.classList.toggle('color');
         })
